aaa691d9fafd15057fa768aa9313b7f491710afd
ed system on behalf of Bob Kummerfeld <ed>
2020-10-11 23:42:24 +0000

    Scaffold update by instructor

---
+++ runner.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: ascii -*-
+
+import sys, os, time, datetime, signal
+
+"""
+The configuration file for runner.py will contain one line for each program that is to be run.   Each line has the following parts: 
+
+timespec program-path parameters
+
+where program-path is a full path name of a program to run and the specified time(s), parameters are the parameters for the program,
+timespec is the specification of the time that the program should be run.
+
+The timespec has the following format:
+
+[every|on day[,day...]] at HHMM[,HHMM] run
+
+Square brackets mean the term is optional, vertical bar means alternative, three dots means repeated.
+
+Examples:
+
+every Tuesday at 1100 run /bin/echo hello
+	every tuesday at 11am run "echo hello"
+on Tuesday at 1100 run /bin/echo hello
+	on the next tuesday only, at 11am run "echo hello"
+every Monday,Wednesday,Friday at 0900,1200,1500 run /home/bob/myscript.sh
+	every monday, wednesday and friday at 9am, noon and 3pm run myscript.sh
+at 0900,1200 run /home/bob/myprog
+	runs /home/bob/myprog every day at 9am and noon
+
+
+"""
+
+#
+# open the configuration file and read the lines, 
+#    check for errors
+#    build a list of "run" records that specifies a time and program to run
+#
+
+#
+# define up the function to catch the USR1 signal and print run records
+#
+
+#
+# sort run records by time
+# take the next record off the list and wait for the time, then run the program
+# add a record to the "result" list
+# if this was an "every" record", add an adjusted record to the "run" list 
+#
+# repeat until no more to records on the "run" list, then exit
+#
+
+
+
+
---
+++ runstatus.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: ascii -*-
+
+import sys, os
+
+pidfile = ".runner-pid"
+statusfilename = ".runner-status"
+
+
+#
+# open the pidfile and read the process id
+#    give an error message if file not found or bad pid
+# send the USR1 signal to runner.py
+# open the status file for reading and check the size
+# wait until it is non zero size, then read contents and copy to output, then quit.
+#
+# give error messages as necessary
+
+
